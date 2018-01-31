477269bb1d5174bd2c88aa9c66db73ef7da703e0
robin.wang <robin.wang@bamboosnetworks.com>
2018-01-31 17:54:51 +0800

    1

---
+++ src/main/java/com/wangrj/TestClass.java
@@ -0,0 +1,22 @@
+package com.wangrj;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
+public class TestClass {
+
+    private Logger logger;
+
+    @Before
+    public void initLogger() {
+        logger = Logger.getLogger("robin");
+    }
+
+    @Test
+    public void testLog() {
+        logger.info("info4234   得分v");
+    }
+
+}
