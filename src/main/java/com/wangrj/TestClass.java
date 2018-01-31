9dcc5e7691bdbee8a6c02313059da50751ef1924
wangrongjun <15521302230@163.com>
2018-02-01 01:04:53 +0800

    aaa

--- src/main/java/com/wangrj/TestClass.java
+++ src/main/java/com/wangrj/TestClass.java
@@ -1,22 +1,28 @@
 package com.wangrj;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.logging.Logger;
-
 public class TestClass {
 
-    private Logger logger;
+    private Logger logger = Logger.getLogger(TestClass.class);
 
     @Before
     public void initLogger() {
-        logger = Logger.getLogger("robin");
+//        BasicConfigurator.configure();
     }
 
     @Test
     public void testLog() {
-        logger.info("info4234   得分v111111");
+        for (int i = 1; i <= 200; i++) {
+            logger.debug("debug_" + i);
+            logger.info("info_" + i);
+            logger.warn("warn_" + i);
+            logger.error("error_" + i);
+            logger.fatal("fatal_" + i);
+        }
     }
 
 }
